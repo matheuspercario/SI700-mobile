ed09116f504633f701e2cb746e7f050124c7ea26
Matheus Percário <matheuspercario@gmail.com>
2021-05-19 16:41:51 -0300

    created node server

---
+++ backend_nodejs/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require("cors")
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.listen(3000);
+
+
+app.get('/', function(req, res){res.send('Hello world')});
+
+/*
+  Servidor propriamente dito
+*/
+
+const notes = [
+    {id: 0, title: "Vinícius Nonato Rodrigues", description : "Aluno da Unicamp feliz"},
+    {id: 1, title: "Carolina da Silva Sancho", description : "Aluna da Unicamp não tão feliz"}
+]
+
+const endpoint = "/notes";
+
+app.get(endpoint, (req, res) => {
+    res.send(notes.filter(Boolean));
+});
+
+app.get(`${endpoint}/:id`, (req, res) => {
+    const id = req.params.id;
+    const note = notes[id];
+
+    if (!note){
+        res.send("{}");
+    } else {
+        res.send(note);
+    }   
+});
+
+app.post(endpoint, (req, res) => {
+    const note = {
+        id : notes.length,
+        title : req.body["title"],
+        description : req.body["description"]
+    };
+    notes.push(note);
+    res.send("1");
+});
+
+app.put(`${endpoint}/:id`, (req, res) => {
+    const id = req.params.id;
+    const note = {
+        id : id,
+        title : req.body["title"],
+        description : req.body["description"]
+    };
+
+    notes[id] = note;
+    res.send("1");
+});
+
+app.delete(`${endpoint}/:id`, (req, res) => {
+    const id = req.params.id;
+    delete notes[id];
+    res.send("1");
+});
